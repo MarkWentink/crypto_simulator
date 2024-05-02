d79726031510bc966f0da778c4842fb206a3cbf3
MarkWentink <43547708+MarkWentink@users.noreply.github.com>
2024-05-02 17:41:28 +0100

    loggin

--- pages/2_Bot_Creator.py
+++ pages/2_Bot_Creator.py
@@ -21,15 +21,15 @@ key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="build-a-bot-traffic-log")
 
-doc_ref = db.collection("bot_creation").document('test2')
-st.write(str(datetime.today()))
-# doc_ref.set({'foo':'bar'})
-doc_ref.set({'timestamp':datetime.today(),
-             'bot_name':'Omega',
-             'strategy':'Hold',
-             'allocation':{'USD':1000},
-             'roi':3.24,
-             'volatility':2.11})
+# doc_ref = db.collection("bot_creation").document('test2')
+# st.write(str(datetime.today()))
+# # doc_ref.set({'foo':'bar'})
+# doc_ref.set({'timestamp':datetime.today(),
+#              'bot_name':'Omega',
+#              'strategy':'Hold',
+#              'allocation':{'USD':1000},
+#              'roi':3.24,
+#              'volatility':2.11})
 
 # Introduction
 st.title("Bot Creator")
@@ -207,7 +207,7 @@ if valid_bot:
         def save_bot(bot, prices, db):
             bot.new_simulate_update(prices)
 
-            doc_ref = db.collection("bot_creation").document(str(datetime.today()))
+            doc_ref = db.collection("bot_creation").document('test3')
             doc_ref.set({'timestamp':datetime.today(),
                          'bot_name':bot_name,
                          'strategy':bot.strategy.description,
